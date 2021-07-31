08fa10e103b4095a7791bcf2f7a2f476c07e0134
Danny-Quan <71638775+Danny-Quan@users.noreply.github.com>
2021-07-31 10:12:10 +0100

    Update edit_notice.php

--- edit_notice.php
+++ edit_notice.php
@@ -83,7 +83,8 @@ foreach($showFullNotice as $cur)
 </div>
 
 
-<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
+<!-- <script type="text/javascript" src="ckeditor/ckeditor.js"></script> -->
+    <script src="https://cdn.ckeditor.com/4.16.1/standard/ckeditor.js"></script>
 <script>
 CKEDITOR.replace('mycontent',{
     filebrowserUploadUrl:"upload.php"
